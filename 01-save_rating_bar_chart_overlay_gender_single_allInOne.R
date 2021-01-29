7420a059b60eae7461d6052b6eea6c81cdc0ea98
ChrisKra <24881066+ChrisKra@users.noreply.github.com>
2021-01-29 15:20:40 +0100

    Files updated

---
+++ 01-save_rating_bar_chart_overlay_gender_single_allInOne.R
@@ -0,0 +1,109 @@
+# usage: 
+# 1. replace first and last column name 
+# 2. replace output name for df
+
+# Alpha function for colors
+add.alpha <- function(col, alpha=1){
+  if(missing(col))
+    stop("Please provide a vector of colours.")
+  apply(sapply(col, col2rgb)/255, 2, 
+        function(x) 
+          rgb(x[1], x[2], x[3], alpha=alpha))  
+}
+
+# REPLACE HERE!!!
+s <- which(colnames(df) == "Autonomie bei der Arbeit") # First column
+e <- which(colnames(df) == "Zusätzliche Leistungen") # Last column
+
+# List of dataframes 
+df_list <- vector("list", 5)
+
+for(rank in 1:5) {
+  # function to convert column to vector
+  rcolumnToVector <- function(rcolumnM, rcolumnW) {
+    tmp = c(0, 0)
+    # Fill M Value 
+    if(!is.na(rcolumnM[toString(rank)])) {
+      tmp[1] = rcolumnM[toString(rank)]*(6-rank) # Weigh in rank
+    }
+    # Fill W Value view 
+    if(!is.na(rcolumnW[toString(rank)])) {
+      tmp[2] = rcolumnW[toString(rank)]*(6-rank) # Weigh in rank
+    }
+    return(tmp)
+  }
+  
+  if(rank == 1) {
+    # convert first column and create df
+    tmp_df <- data.frame(rcolumnToVector(table(df %>% filter(df[["Geschlecht"]] == "Männlich") %>% select(s)), table(df %>% filter(df[["Geschlecht"]] == "Weiblich") %>% select(s)))) # create df with first column
+    names(tmp_df)[1] <- colnames(df)[s] # rename column
+    
+    # append all other columns
+    for(i in 2:(e-s+1)){
+      tmp_df[i] = rcolumnToVector(table(df %>% filter(df[["Geschlecht"]] == "Männlich") %>% select(s+i-1)), table(df %>% filter(df[["Geschlecht"]] == "Weiblich") %>% select(s+i-1)))
+      names(tmp_df)[i] <- colnames(df)[s+i-1]
+    }
+  } else {
+    # append all other columns
+    for(i in 1:(e-s+1)){
+      tmp <- rcolumnToVector(table(df %>% filter(df[["Geschlecht"]] == "Männlich") %>% select(s+i-1)), table(df %>% filter(df[["Geschlecht"]] == "Weiblich") %>% select(s+i-1)))
+      tmp_df[i] <- tmp_df[i] + tmp # Add to existing column
+    }
+  }
+  
+  # name rows 
+  row.names(tmp_df) <- c("Männlich", "Weiblich")
+}
+
+# convert table to percent 
+sums <- rowSums(tmp_df);
+for(i in 1:2) {
+  for(j in 1:ncol(tmp_df)) {
+    tmp_df[i, j] = (tmp_df[i, j]/sums[i])
+  }
+}
+
+# Export tables as CSV
+write.table(round(tmp_df, 4), file = "CSV_exports/Prio_Single_Split.csv", row.names = FALSE, col.names = FALSE, sep=";")
+
+
+# Add maximum row
+# tmp_df <- rbind(rep(max(tmp_df),(e-s+1)) , rep(0,(e-s+1)) , tmp_df)
+
+# generate graph
+
+# transpose
+tmp_df <- t(tmp_df)
+
+# prepare for ggplot
+tmp_df <- melt(tmp_df,"Row.names")
+colnames(tmp_df)[2] <- "Geschlecht"
+colorM = "#31aad9"
+colorW = "#b83578"
+
+png(file="Generated_Charts/Prio_Bar_Chart_single.png",width=1400, height=900)
+
+par(oma=c(2,2,2,2))
+
+print(
+  ggplot(tmp_df, aes(x = Row.names, y = value)) +
+    geom_bar(aes(fill = Geschlecht), stat = "identity", position = "dodge", alpha = 0.9) +
+    labs(x = "Charakteristik", y = "Gewichtete Verteilung") +
+    theme_minimal() +
+    scale_fill_manual(values=c(colorM,colorW)) + 
+    ggtitle("Prioritäten kombiniert") + 
+    theme(plot.margin = margin(2, 2, 1, 2, "cm"),
+          text = element_text(size=20),
+          axis.text.x = element_text(angle = -40, hjust = .1), 
+          axis.title.y = element_text(vjust = 4),
+          plot.title = element_text(hjust = 0.5, vjust=4, face="bold"))
+)
+
+dev.off()
+
+# Debugging
+# view(tmp_df)
+# rowSums(tmp_df)
+# arbeitgeber_df <- tmp_df # REPLACE HERE!!!
+
+rm(df_list, tmp_df, tmp, colorM, colorW, s, e, i, j, rank, sums)
---
+++ 02_1-save_CarAnch_charts_individual_grouped.R
@@ -0,0 +1,104 @@
+# First and last question
+s <- which(colnames(df) == "Karriere [Der Prozess der Beaufsichtigung, Beeinflussung, Führung und Kontrolle von Menschen ist...]") # First column
+e <- which(colnames(df) == "Karriereplanung [Ich wollte schon immer ein eigenes Unternehmen gründen und aufbauen.]") # Last column
+
+exclude <- which(colnames(df) == "Karriere [In meinem Fachgebiet zu bleiben im Gegensatz zur Beförderung außerhalb meines Fachgebietes ist...]") # Exclude column with wrong translation 
+
+# Maxmimum answer value (5 for Karriereorientierung 1 and 7 for Karriereorientierung 2)
+maxValue <- 5
+
+nColumns <- (e-s+1)
+df_list <- vector("list", nColumns)
+
+# Function to convert column to vector
+rcolumnToVector <- function(rcolumn) {
+  tmp = rep(0, maxValue)
+  for(j in 1:maxValue) {
+    if(!is.na(rcolumn[j])) {
+      tmp[j] = rcolumn[j]
+    }
+  }
+  return(tmp)
+}
+
+for(colNum in 1:nColumns) {
+  # can be replaced with more friedly name 
+  tmp_df <- data.frame(rank = c(1:maxValue), m = rep(0, maxValue), w = rep(0, maxValue))
+  if ((s+colNum-1) != -1) { # replace -1 with exclude
+    tmp_df$m <- rcolumnToVector(prop.table(table(df %>% filter(df[["Geschlecht"]] == "Männlich") %>% select(s+colNum-1))))
+    tmp_df$w <- rcolumnToVector(prop.table(table(df %>% filter(df[["Geschlecht"]] == "Weiblich") %>% select(s+colNum-1))))
+  } else {
+    tmp_df$m <- rcolumnToVector(prop.table(table(df %>% filter(df[["Start-Sprache"]] == "de" & df[["Geschlecht"]] == "Männlich") %>% select(s+colNum-1))))
+    tmp_df$w <- rcolumnToVector(prop.table(table(df %>% filter(df[["Start-Sprache"]] == "de" & df[["Geschlecht"]] == "Weiblich") %>% select(s+colNum-1))))
+  }
+  
+
+  df_list[[colNum]] <- tmp_df
+}
+
+# Group individual tables by Factor
+mapping <- data.frame(c(13, 17, 25), c(10, 11, 20), c(8, 9, 22), c(1, 2, 3), c(15, 19, 24), c(14, 18, NA), c(6, 7, 21), c(4, 5, NA), c(12, 16, 23))
+desc_de <- c("Unternehmerische Kreativität", "Technische/funktionale Kompetenz", "Serviceorientierung/Sinnstiftung", "General-Management-Kompetenz", "Lebensstil", "Sicherheit-Geografisch", "Autonomie/Unabhängigkeit", "Sicherheit-Arbeitsplatzsicherheit", "Reine Herausforderung")
+desc_en <- c("Entrepreneurship", "Technical", "Service", "Managerial", "Lifestyle", "Security-Geographic", "Autonomy", "Security-Job tenure", "Pure challange")
+desc <- desc_en
+
+grouped_list <- vector("list", length(desc))
+for(i in 1:length(desc)) {
+  if (!is.na(mapping[3, i])) {
+    grouped_list[[i]] <- (df_list[[mapping[1, i]]] + df_list[[mapping[2, i]]] + df_list[[mapping[3, i]]])/3
+  } else {
+    grouped_list[[i]] <- (df_list[[mapping[1, i]]] + df_list[[mapping[2, i]]])/2
+  }
+  
+  # Export tables as CSV
+  write.table(round(grouped_list[[i]], 4), file = paste("CSV_exports/CarAnch_", i, "-", str_replace_all(desc[i], "/", "_"), ".csv", sep=""), row.names = FALSE, sep=";")
+}
+
+# Graph settings
+smoothingAmount = 7.5/10
+colorM = "#31aad9"
+colorW = "#b83578"
+areaAlpha = 0.4
+lineSize = 1.5
+indWidth = 500
+indHeight = 500
+
+# Function to wrap sentences to fit width
+wrap_sentence <- function(string, width) {
+  words <- unlist(strsplit(string, " "))
+  fullsentence <- ""
+  checklen <- ""
+  for(i in 1:length(words)) {
+    checklen <- paste(checklen, words[i])
+    if(nchar(checklen)>(width+1)) {
+      fullsentence <- paste0(fullsentence, "\n")
+      checklen <- ""
+    }
+    fullsentence <- paste(fullsentence, words[i])
+  }
+  fullsentence <- sub("^\\s", "", fullsentence)
+  fullsentence <- gsub("\n ", "\n", fullsentence)
+  return(fullsentence)
+}
+
+for(p in 1:length(desc)) {
+  png(file=paste("Generated_Charts/CarAnch_Charts-", p, "-", str_replace_all(desc[p], "/", "_"), ".png", sep = ""),width=indWidth, height=indHeight)
+  
+  print(ggplot(grouped_list[[p]], aes(x=rank)) +
+          stat_smooth(geom = 'area', method = 'loess', span = smoothingAmount, aes(y = m), fill=colorM, alpha=areaAlpha) +
+          stat_smooth(geom = 'line', method = 'loess', span = smoothingAmount, aes(y = m), color=colorM, size=lineSize) +
+          stat_smooth(geom = 'area', method = 'loess', span = smoothingAmount, aes(y = w), fill=colorW, alpha=areaAlpha) +
+          stat_smooth(geom = 'line', method = 'loess', span = smoothingAmount, aes(y = w), color=colorW, size=lineSize) +
+          scale_x_continuous(labels=c("Nicht wichtig", "Mäßig wichtig", "Etwas wichtig", "Sehr wichtig", "Von zentraler\nBedeutung")) +
+          labs(x = "Relevanz", y = "Anteil") +
+          ggtitle(wrap_sentence(desc[p], 60)) +
+          theme_minimal() +
+          theme(plot.margin = margin(2.5, 1.5, 1.5, 1, "cm"), 
+                plot.title = element_text(hjust = 0.5, vjust = 6, size = 15, face = "bold")))
+  
+  # print(paste("Generating graph", p))
+  dev.off()
+}
+
+# Clean up 
+rm(df_list, tmp_df, areaAlpha, colNum, colorM, colorW, e, indHeight, indWidth, lineSize, maxValue, nColumns, p, s, smoothingAmount, desc, desc_de, desc_en, i, grouped_list, mapping)
